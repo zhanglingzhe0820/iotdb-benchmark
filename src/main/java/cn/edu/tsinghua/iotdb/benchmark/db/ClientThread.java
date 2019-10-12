baa1d8e9d2c3f1f9daa726b464b158c5090e4390
nlosilva1 <thss15_yit@163.com>
2019-10-13 01:08:06 +0800

    chore(workload): remove log

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/ClientThread.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/ClientThread.java
@@ -204,8 +204,9 @@ public class ClientThread implements Runnable {
           loopRate = pointsOneLoop / (loopSecond + delayTime * 0.000000001d);
         }
 
+        /*
         LOGGER.info("{} LOOP ,{}, INSERT RATE,{},points/s,LOOP DELTA TIME,{},second",
-            Thread.currentThread().getName(), i, loopRate, loopSecond);
+            Thread.currentThread().getName(), i, loopRate, loopSecond);*/
         mySql.saveInsertProcessOfLoop(i, loopRate);
       }
     }
